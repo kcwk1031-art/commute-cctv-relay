39528f2e490dd8bdb8e79286b8636a5c0be02d98
kcwk1031-art <kcwk1031@gmail.com>
2026-09-11 14:52:05 +0800

    Add files via upload

---
+++ drive-config.js
@@ -0,0 +1,5 @@
+// This value is public. Never put traffic-data credentials in this file.
+window.DRIVE_CONFIG = {
+  cctvProxyBase: "https://commute-cctv-relay.onrender.com",
+  cctvRelayBase: "https://commute-cctv-relay.onrender.com",
+};
